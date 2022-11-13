5038256a33c496b2b35c21c04002453a1a121ef2
Geedorah <jbormann54@gmail.com>
2022-11-13 00:16:09 -0600

    first functional commit

--- Functional.js
+++ Functional.js
@@ -1,15 +1,21 @@
 
-const workouts = ['Kettelbell Swings 4 x 30', '2 mile ruck with weighted vest','30 minute walk on incline treadmil','50 burpees','5 15m sprints','15 minutes on the assault bike','10 minutes of farmers carrys','25 box jumps']
+const workouts = ['Kettelbell Swings 4x30', '2 mile ruck with weighted vest','30 minute walk on incline treadmil','50 burpees','5 15m sprints','15 minutes on the assault bike','10 minutes of farmers carrys','25 box jumps']
 let workout 
-const findWorkout = () => {
+let message
+
+function findWorkout() {
     let rand = Math.floor(Math.random()*workouts.length)
     workout = workouts[rand]
     return workout
 }
 
-const displayMessage = () => {
+function displayMessage() {
     let final = findWorkout()
-    return `Your cardio session today will be complete with: ${final}`
+    message = `Your cardio session today will be complete with: ${final}`
+    return message
 }
 
-button.onclick = displayMessage
+function changeText() {
+    document.getElementById('Output').innerHTML = displayMessage()
+}
+
